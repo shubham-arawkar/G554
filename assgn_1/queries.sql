3b8d29bd65713b0dde9ee00508b4268af8df4c50
Shubham Arawkar <shubham.arawkar.tss@gmail.com>
2020-08-24 21:14:30 +0530

    [ADD] error description in queries.sql

--- assgn_1/queries.sql
+++ assgn_1/queries.sql
@@ -1,10 +1,13 @@
 -- INSERT INTO employee VALUES ('Cecilia', 'F', 'Kolonsky', NULL, '1960-04-05', '6357 Windy Lane, Katy, TX', 'F', 28000, NULL, 4);
+-- #1048 - Column 'ssn' cannot be null [PRIMARY KEY can't be null]
 
 INSERT INTO employee VALUES ('Alicia', 'J', 'Zelaya', '999887777', '1960-04-05', '6357 Windy Lane, Katy, TX', 'F', 28000, '987654321', 4);
 
 -- INSERT INTO employee VALUES ('Cecilia', 'F', 'Kolonsky', '677678989', '1960-04-05', '6357 Windswept, Katy, TX', 'F', 28000, '987654321', 7);
+-- #1062 - Duplicate entry '677678989' for key 'PRIMARY'
 
 -- INSERT INTO employee VALUES ('Cecilia', 'F', 'Kolonsky', '677678989', '1960-04-05', '6357 Windy Lane, Katy, TX', 'F', 28000, NULL, 4);
+-- #1062 - Duplicate entry '677678989' for key 'PRIMARY'
 
 DELETE FROM works_on WHERE essn = '999887777' and pno = 10;
 
